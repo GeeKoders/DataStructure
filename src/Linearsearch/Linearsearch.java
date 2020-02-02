4b27a1d2a530b00346a826bf9d9d45dfd650cd0f
GeeKoders <iloveitone@gmail.com>
2020-02-02 12:48:15 +0800

    20200202 Linear search

---
+++ src/Linearsearch/Linearsearch.java
@@ -0,0 +1,21 @@
+package Linearsearch;
+
+public class Linearsearch {
+
+	public static void main(String[] args) {
+		
+		int [] nums = {1, 4, 5, 6, 10, -4, 67, 100} ;
+		
+		System.out.println(nums[2]) ; // O(1) we know the index
+		
+		for(int i=0; i<nums.length; i++){
+			
+			if(nums[i] == 10){
+				System.out.println("Index of the item we are looking for is:" + i);
+			}
+			
+			// O(N)
+		}
+	}
+
+}
