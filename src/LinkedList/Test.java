35f3b1048c9d598075bf74e1b8715ea0260090c1
GeeKoders <iloveitone@gmail.com>
2020-02-03 22:20:50 +0800

    20200203 reversing an array in-place

---
+++ src/LinkedList/Test.java
@@ -0,0 +1,26 @@
+package LinkedList;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Test {
+
+	public static void main(String[] args) {
+		int num1, num2, num3;
+
+		Scanner sc = new Scanner(System.in);
+		System.out.println("請輸入三個數字:");
+
+		num1 = sc.nextInt();
+		num2 = sc.nextInt();
+		num3 = sc.nextInt();
+
+		System.out.println("您輸入第一個數字為：" + num1);
+		System.out.println("您輸入第二個數字為：" + num2);
+		System.out.println("您輸入第三個數字為：" + num3);
+
+	}
+
+}
