1ca4f8f86b668bdba349313208181c7ce0ee339c
GeeKoders <iloveitone@gmail.com>
2020-02-05 08:08:11 +0800

    20200205 Reservoir problem

--- src/Arrays/Test.java
+++ src/Arrays/Test.java
@@ -12,6 +12,7 @@ public class Test {
 		// anagramProblem() ;
 		// repeatedIntegerProblem() ;
 		// lottoDraw() ;
+		 reservoirProblem() ;
 
 		
 	}
@@ -109,6 +110,29 @@ public class Test {
 		}
 		for (int i = 0; i < 6; i++) {
 			System.out.print(num[i] + " ");
+		}
+	}
+	
+	public static void reservoirProblem(){
+		int []num ={13, 65, 46, 11, 71, 50, 12, 5, 81, 97, 28, 74, 87, 68, 42, 31, 40, 37, 43, 25} ;
+		int k =5 ;
+		
+		
+		int [] reservoir = new int [k] ;
+		// 1. copy 
+		for(int i=0; i<reservoir.length; i++){
+			reservoir[i] = num[i] ;
+		}
+		
+		Random random = new Random() ;
+		
+		for(int i=k+1; i<num.length; i++){
+			int j = random.nextInt(i) ;
+			if(j<k) reservoir[j] = num[i] ;
+		}
+		
+		for(int i=0; i<reservoir.length; i++){
+			System.out.print(reservoir[i] + " ") ;
 		}
 	}
 }
