14f658ce5e5a99cdf35f35b9459c02eb00ae53f9
GeeKoders <iloveitone@gmail.com>
2020-02-08 13:06:18 +0800

    20200208 Linkedlist insert middle

--- src/LinkedList/Test.java
+++ src/LinkedList/Test.java
@@ -12,45 +12,33 @@ public class Test {
 	private TestNode last ;
 	
 	public static void main(String[] args) {
-//		 linkedListInserHead() ;
-//		linkedListInsertTail() ;
-		
-		
-	}
-	
-	public static void linkedListInserHead(){
-		
-		Scanner scanner = new Scanner(System.in) ;
-		
 		Test test = new Test() ;
+
+//		test.insertH(1) ; 
+//		test.insertH(2) ;
+//		test.insertH(3) ; 
+//		test.insertH(4) ;
+//		test.insertH(5) ;
+//		
+//		test.print() ; // 5 4 3 2 1
 		
-		for(int i=0; i<5; i++){
-			
-			System.out.print("Please input your score:");
-			int score = scanner.nextInt() ;
-			test.insertH(score);
-		}
+//		test.insertT(1) ;
+//		test.insertT(2) ;
+//		test.insertT(3) ;
+//		test.insertT(4) ;
+//		test.insertT(5) ;
+//		test.print() ; // 1 2 3 4 5
+//		System.out.println("==================================") ;
+//		
+//		test.insertM(6, 3) ;
+//		test.print() ; // 1 2 3 6 4 5
 		
-		test.print();
+		
+		
+		
 	}
 	
-	public static void linkedListInsertTail(){
-		
-		Scanner scanner = new Scanner(System.in) ;
-		
-		Test test = new Test() ;
-		
-		for(int i=0; i<5; i++){
-			
-			System.out.print("Please input your score:");
-			int score = scanner.nextInt() ;
-			test.insertT(score);
-		}
-		
-		test.print();
-	}
-	
-	
+
 	public void insertH(int data){
 		
 		TestNode newNode = new TestNode(data) ;
@@ -76,6 +64,31 @@ public class Test {
 			last.next = newNode ; 
 			last = newNode ;
 		}
+		
+	}
+	
+	public void insertM(int data, int afterData){
+		
+		TestNode newNode = new TestNode(data) ;
+		TestNode current = first ;
+		boolean searchFlag = false ;
+		
+		if(isEmpty()){
+			first = newNode ;
+			last = newNode ;
+		}else{
+			while(current.next!=null && current.data != afterData){
+				current = current.next ;
+				if(current.data == afterData){
+					searchFlag = true ;
+					newNode.next = current.next ;
+					current.next = newNode ;
+				}
+					
+			}
+		}
+		
+		if(!searchFlag) System.out.println("It can't find target value:" + afterData);
 		
 	}
 	
