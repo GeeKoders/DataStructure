802fdf863bf3d656f861dba7a72d03ffc266397b
GeeKoders <iloveitone@gmail.com>
2020-02-11 20:25:18 +0800

    20200211 Double LinekedList remove

--- src/LinkedList/DoubleLinkedList.java
+++ src/LinkedList/DoubleLinkedList.java
@@ -24,17 +24,23 @@ public class DoubleLinkedList {
 		doubleLinkedList.insertT(4);
 		doubleLinkedList.insertT(5);
 
+		// doubleLinkedList.print();
+		//
+		// System.out.println("size:" + doubleLinkedList.size());
+		//
+		// doubleLinkedList.insertM(6, 3);
+		//
+		// doubleLinkedList.print();
+
+		System.out.println("===========================");
+
+		// doubleLinkedList.removeH();
+
+		// doubleLinkedList.removeT();
+
+		doubleLinkedList.removeM(0);
 		doubleLinkedList.print();
 
-		System.out.println("size:" + doubleLinkedList.size());
-
-		doubleLinkedList.insertM(6, 3);
-		
-		doubleLinkedList.print();
-		
-		
-		
-		
 	}
 
 	public boolean isEmpty() {
@@ -68,16 +74,16 @@ public class DoubleLinkedList {
 
 		if (isEmpty()) {
 			first = newNode;
-			first.rNext = last ;
+			first.rNext = last;
 			last = newNode;
-			last.lNext = first ;
-//			newNode.lNext = null; redundant
-//			newNode.rNext = null; redundant
+			last.lNext = first;
+			// newNode.lNext = null; redundant
+			// newNode.rNext = null; redundant
 		} else {
 			first.lNext = newNode;
 			newNode.rNext = first;
 			first = newNode;
-			//newNode.lNext = null; redundant
+			// newNode.lNext = null; redundant
 		}
 
 	}
@@ -88,46 +94,108 @@ public class DoubleLinkedList {
 
 		if (isEmpty()) {
 			first = newNode;
-			first.rNext = last ;
+			first.rNext = last;
 			last = newNode;
-			last.lNext = first ;
-//			newNode.lNext = null; redundant
-//			newNode.rNext = null; redundant
+			last.lNext = first;
+			// newNode.lNext = null; redundant
+			// newNode.rNext = null; redundant
 		} else {
 			newNode.lNext = last;
 			last.rNext = newNode;
 			last = newNode;
-//			newNode.rNext = null; redundant
+			// newNode.rNext = null; redundant
 		}
 	}
 
 	public void insertM(int data, int target) {
-		
+
 		DoubleNode newNode = new DoubleNode(data);
-		DoubleNode current =first ;
-		DoubleNode r = null ;
-		
+		DoubleNode current = first;
+		DoubleNode r = null;
+
 		if (isEmpty()) {
 			first = newNode;
-			first.rNext = last ;
+			first.rNext = last;
 			last = newNode;
-			last.lNext = first ;
-//			newNode.lNext = null; redundant
-//			newNode.rNext = null; redundant
+			last.lNext = first;
+			// newNode.lNext = null; redundant
+			// newNode.rNext = null; redundant
 		} else {
-			while(current!=null && current.data!=target){
-				current = current.rNext ;
+			while (current != null && current.data != target) {
+				current = current.rNext;
 			}
-			
-			r = current.rNext ;
-			newNode.rNext = r ;
-			r.lNext = newNode ;
-			current.rNext = newNode ;
-			newNode.lNext = current ;
+
+			r = current.rNext;
+			newNode.rNext = r;
+			r.lNext = newNode;
+			current.rNext = newNode;
+			newNode.lNext = current;
 		}
-		
-		
+
 	}
+
+	public void removeH() {
+
+		if (isEmpty()) {
+			System.out.println("No node to remove");
+		} else {
+			DoubleNode current = first;
+			first = current.rNext;
+			current.rNext = last.rNext;
+		}
+	}
+
+	public void removeT() {
+
+		if (isEmpty()) {
+			System.out.println("No node to remove");
+		} else {
+			DoubleNode current = first;
+			DoubleNode prev = first;
+			while (current != last) {
+				prev = current;
+				current = current.rNext;
+			}
+			if (first != last) {
+				prev.rNext = last.rNext;
+				last.lNext = first.lNext;
+				last = prev;
+			} else {
+				first = current.lNext;
+				last = current.rNext;
+			}
+
+		}
+	}
+
+	public void removeM(int target) {
+
+		if (isEmpty()) {
+			System.out.println("No node to remove");
+		} else {
+			DoubleNode current = first;
+			DoubleNode prev = first;
+			DoubleNode r = first;
+			while (current != null && current.data != target) {
+				prev = current;
+				current = current.rNext;
+			}
+			if (current == null) {
+				System.out.println("It can't find the target value:" + target);
+			} else if (current != first && current != last) {
+				r = current.rNext;
+				r.lNext = current.lNext;
+				prev.rNext = current.rNext;
+				current.rNext = last.rNext;
+				current.lNext = first.lNext;
+			} else {
+				System.out.println("The target value " + target
+						+ " is not in the middle");
+			}
+
+		}
+	}
+
 }
 
 class DoubleNode {
