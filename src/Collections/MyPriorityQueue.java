8a9c709f94771a6aa2f4eb566d5ec38abb5a3a25
GeeKoders <iloveitone@gmail.com>
2020-02-28 12:21:58 +0800

    20200228 add MyPriorityQueue comment

--- src/Collections/MyPriorityQueue.java
+++ src/Collections/MyPriorityQueue.java
@@ -5,6 +5,25 @@ import java.util.Queue;
 
 public class MyPriorityQueue {
 
+	
+	/**
+	 * Base on a priority heap !!!
+	 * 	 we assign a priority value to every single items
+	 * 
+	 * 		- element with high priority is served before an elements with low priority
+	 * 
+	 * 		- the elements of the priority queue are ordered according to their natural ordering 
+	 * 			defined by the Comparable interface
+	 * 
+	 * add() -> add elements to the queue
+	 * 
+	 * peek() -> Retrieves, but does not remove, the head of this queue,
+	 * 				or returns null if this queue is empty.
+	 * 
+	 * poll() -> Retrieves and removes the head of this queue, or returns null if this queue is empty
+	 * 
+	 */
+	
 	public static void main(String[] args) {
 
 		Queue queue = new PriorityQueue();
