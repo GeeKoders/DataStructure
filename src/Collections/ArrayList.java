81183ee1af78d0792900cd89b01f87beceac16d2
GeeKoders <iloveitone@gmail.com>
2020-03-01 15:46:41 +0800

    20200301 add Collections ArrayList

---
+++ src/Collections/ArrayList.java
@@ -0,0 +1,50 @@
+package Collections;
+
+import java.util.List;
+
+public class ArrayList {
+
+	/*
+	 *  This is one of the implementation of the List interface !!!
+	 *  	An array stores the values in the background
+	 * 
+	 * 		int [] array = new int[10] ;
+	 * 
+	 *  - random access / very fast O(1) --> random access: accessing items by the index -list.get(0)
+	 *  
+	 *  - all operations run in O(N) time approximately
+	 *  
+	 *  - but if we want to remove items, this operation is not so efficient
+	 *			We have to shift each item in out list - O(N)  		
+	 * 
+	 *  If our application is just about adding elements to a list and 
+	 *  	getting elements with known indexes -> an arrayList is perfects
+	 * 
+	 *  capacity: it is a parameter we can define when we instantiate an ArrayList
+	 *  	It is the initial size of the array
+	 *  		 - if we know that we will store 5 million items in a list we should use it
+	 *  				 list = new ArrayList(5000000) WHY? This may reduce the amount of incremental reallocation.
+	 * 
+	 */
+	
+	
+	public static void main(String[] args) {
+		
+		List<String> list = new java.util.ArrayList<>(10) ;
+		
+		list.add("Adam") ;
+		list.add("Joe") ;
+		list.add("Kevin") ;
+		
+		list.remove(0) ;
+		
+		Object []array = list.toArray() ;
+		
+		for(Object s: array){
+			System.out.println(s) ;
+		}
+		
+		
+		
+	}
+}
