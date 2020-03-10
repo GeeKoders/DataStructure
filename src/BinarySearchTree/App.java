a627fb9d7621971e6edfbe30f6aed30264e29556
GeeKoders <iloveitone@gmail.com>
2020-03-10 22:21:39 +0800

    20200310 add custom object in a tree

--- src/BinarySearchTree/App.java
+++ src/BinarySearchTree/App.java
@@ -4,14 +4,24 @@ public class App {
 	
 	public static void main(String[] args) {
 		
-		Tree<Integer> bst = new BinarySearchTree<>() ;
+//		Tree<Integer> bst = new BinarySearchTree<>() ;
+//		
+//		bst.insert(10) ;
+//		bst.insert(5) ;
+//		bst.insert(15) ;
+//		bst.insert(3) ;
+//		
+//		bst.delete(5);
+//		bst.traversal();
+		// O(N) or O(logN)
+		Tree<Person> bst = new BinarySearchTree<Person>() ;
 		
-		bst.insert(10) ;
-		bst.insert(5) ;
-		bst.insert(15) ;
-		bst.insert(3) ;
+		bst.insert(new Person("Adam", 27));
+		bst.insert(new Person("Kevin", 13));
+		bst.insert(new Person("Joe", 67));
+		bst.insert(new Person("Michael", 2));
+		bst.insert(new Person("Smith", 11));
 		
-		bst.delete(5);
 		bst.traversal();
 		
 	}
