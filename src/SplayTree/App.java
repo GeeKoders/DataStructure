47b290a400cbe0f1c850a3ec4dad9ef5f971b2f3
GeeKoders <iloveitone@gmail.com>
2020-04-05 17:26:44 +0800

    20200405 add splay tree test

--- src/SplayTree/App.java
+++ src/SplayTree/App.java
@@ -3,7 +3,21 @@ package SplayTree;
 public class App {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Tree<Integer> splayTree = new SplayTree<>() ;
+		
+		splayTree.insert(10);
+		splayTree.insert(-5);
+		splayTree.insert(0);
+		splayTree.insert(20);
+		splayTree.insert(30);
+		
+		
+//		System.out.println(splayTree.getMax());
+//		splayTree.inOrderTraversal();
+		splayTree.find(20) ;
+		
+		((SplayTree) splayTree).printRoot() ;
 
 	}
 
