5f78ee25f8cf165eba18e1c0c00c67c985fa93a3
GeeKoders <iloveitone@gmail.com>
2020-04-12 17:15:52 +0800

    20200412 add checkHeap

--- src/Heap/APP.java
+++ src/Heap/APP.java
@@ -3,29 +3,35 @@ package Heap;
 public class APP {
 
 	public static void main(String[] args) {
-		
-		Heap heap = new Heap() ;
-		
-		heap.insert(10);
-		heap.insert(8);
-		heap.insert(12);
-		heap.insert(20);
-		heap.insert(-2);
-		heap.insert(0);
-		heap.insert(1);
-		heap.insert(321);
-		
-//		System.out.println(heap.poll());
-//		System.out.println(heap.poll());
-//		System.out.println(heap.poll());
-//		System.out.println(heap.poll());
-//		System.out.println(heap.poll());
-//		System.out.println(heap.poll());
-//		System.out.println(heap.poll());
-//		System.out.println(heap.poll());
-		
-		
-		heap.heapsort() ;
+
+		// Heap heap = new Heap() ;
+		//
+		// heap.insert(10);
+		// heap.insert(8);
+		// heap.insert(12);
+		// heap.insert(20);
+		// heap.insert(-2);
+		// heap.insert(0);
+		// heap.insert(1);
+		// heap.insert(321);
+
+		// System.out.println(heap.poll());
+		// System.out.println(heap.poll());
+		// System.out.println(heap.poll());
+		// System.out.println(heap.poll());
+		// System.out.println(heap.poll());
+		// System.out.println(heap.poll());
+		// System.out.println(heap.poll());
+		// System.out.println(heap.poll());
+
+		// heap.heapsort() ;
+
+		CheckHeap checkHeap = new CheckHeap();
+
+		int[] array = { 10, 14, 19, 26, 31, 42, 27, 44, 35, 33, 35 };
+
+		System.out.println(checkHeap.isMinHeap(array));
+
 	}
 
 }
