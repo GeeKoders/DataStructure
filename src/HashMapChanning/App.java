df2178bb87d29c578b28570c4a375de7ac299cff
GeeKoders <iloveitone@gmail.com>
2020-04-20 20:45:33 +0800

    20200420 add HashTable test

--- src/HashMapChanning/App.java
+++ src/HashMapChanning/App.java
@@ -3,7 +3,18 @@ package HashMapChanning;
 public class App {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		HashTable hashTable = new HashTable() ;
+		System.out.println();
+		hashTable.put(1, 10);
+		System.out.println();
+		hashTable.put(2, 100);
+		System.out.println();
+		hashTable.put(3, 1000);
+		System.out.println();
+		hashTable.put(4, 10000);
+		
+		System.out.println(hashTable.get(3));
 
 	}
 
--- src/HashMapChanning/HashTable.java
+++ src/HashMapChanning/HashTable.java
@@ -3,6 +3,10 @@ package HashMapChanning;
 public class HashTable {
 
 	private HashItem[] hashTable;
+	
+	public HashTable(){
+		hashTable = new HashItem[Constants.TABLE_SIZE] ;
+	}
 
 	public int get(int key) {
 
@@ -33,20 +37,26 @@ public class HashTable {
 		int hashArrayIndex = hash(key);
 
 		if (hashTable[hashArrayIndex] == null) {
+			System.out.println("No collision simple insertion...");
 			hashTable[hashArrayIndex] = new HashItem(key, value);
 		} else {
 
+			System.out.println("Collision when inserting with key " + key);
 			HashItem hashItem = hashTable[hashArrayIndex];
 
 			while (hashItem.getNextHashItem() != null) {
 				hashItem = hashItem.getNextHashItem();
+				System.out.println("Considering the next item in linked list " + hashItem.getValue());
 			}
+			
+			System.out.println("Finally we have found the place to insert...");
 			hashItem.setNextHashItem(new HashItem(key, value));
 		}
 	}
 
 	private int hash(int key) {
 		return key % Constants.TABLE_SIZE;
+//		return 1 ;
 	}
 
 }
