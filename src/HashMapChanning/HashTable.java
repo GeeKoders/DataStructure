1481ee6e3b7dd1ea71f8f3ad64a36f64b709411c
GeeKoders <iloveitone@gmail.com>
2020-04-20 20:36:15 +0800

    20200420 add HashTable get method

--- src/HashMapChanning/HashTable.java
+++ src/HashMapChanning/HashTable.java
@@ -1,31 +1,52 @@
 package HashMapChanning;
 
 public class HashTable {
-	
-	private HashItem[] hashTable ;
-	
-	public void put(int key, int value){
-		
-		int hashArrayIndex = hash(key) ;
-		
-		if(hashTable[hashArrayIndex] == null){
-			hashTable[hashArrayIndex] = new HashItem(key, value) ;
-		}else{
-			
-			HashItem hashItem = hashTable[hashArrayIndex] ;
-			
-			while(hashItem.getNextHashItem() !=null){
-				hashItem = hashItem.getNextHashItem() ;
+
+	private HashItem[] hashTable;
+
+	public int get(int key) {
+
+		int generatedArrayIndex = hash(key);
+
+		if (hashTable[generatedArrayIndex] == null) {
+			return -1;
+		} else {
+
+			HashItem hashItem = hashTable[generatedArrayIndex];
+
+			while (hashItem != null && hashItem.getKey() != key) {
+				hashItem = hashItem.getNextHashItem();
+			}
+
+			if (hashItem == null) {
+				return -1;
+			} else {
+				return hashItem.getValue();
+			}
+
+		}
+
+	}
+
+	public void put(int key, int value) {
+
+		int hashArrayIndex = hash(key);
+
+		if (hashTable[hashArrayIndex] == null) {
+			hashTable[hashArrayIndex] = new HashItem(key, value);
+		} else {
+
+			HashItem hashItem = hashTable[hashArrayIndex];
+
+			while (hashItem.getNextHashItem() != null) {
+				hashItem = hashItem.getNextHashItem();
 			}
 			hashItem.setNextHashItem(new HashItem(key, value));
 		}
 	}
-	
-	private int hash(int key){
-		return key % Constants.TABLE_SIZE ;
+
+	private int hash(int key) {
+		return key % Constants.TABLE_SIZE;
 	}
-	
-	
-	
 
 }
