0bb0ad1b062c2f4d9e724f8fce2347b25b60090f
GeeKoders <iloveitone@gmail.com>
2020-04-25 12:02:52 +0800

    202004025 add HashMapLinearProbing get method

--- src/HashMapLinearProbing/HashTable.java
+++ src/HashMapLinearProbing/HashTable.java
@@ -8,6 +8,26 @@ public class HashTable {
 		this.hashTable = new HashItem[Constants.TABLE_SIZE] ;
 	}
 	
+	public int get(int key){
+		
+		int generatedIndex = hashFunction(key) ;
+		
+		while(hashTable[generatedIndex] != null && hashTable[generatedIndex].getKey() == key){
+			generatedIndex = (generatedIndex + 1) % Constants.TABLE_SIZE ;  
+		}
+		
+		if(hashTable[generatedIndex] == null){
+			return -1 ;
+		}else{
+			return hashTable[generatedIndex].getValue() ;
+		}
+		
+		
+		
+		
+	}
+	
+	
 	public void put(int key, int value){
 		int generatedIndex = hashFunction(key) ;
 		
