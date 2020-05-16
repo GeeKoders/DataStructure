100226d92718ced27d3fc51341d952a7db78fdae
GeeKoders <iloveitone@gmail.com>
2020-05-16 19:42:06 +0800

    20200516 add Mergesort

---
+++ src/Mergesort/App.java
@@ -0,0 +1,25 @@
+package Mergesort;
+
+import java.util.Random;
+
+public class App {
+	
+	public static void main(String[] args) {
+		
+		Random random = new Random() ;
+		
+		int [] nums = new int[30] ;
+		
+		for(int j=0; j<nums.length; j++){
+			nums[j] = random.nextInt(1000) - 500 ;
+		}
+		
+		// O(nlogn)
+		Mergesort mergesort = new Mergesort(nums) ;
+		mergesort.mergeSort(0, nums.length-1);
+		mergesort.showResult();
+		
+		
+	}
+
+}
