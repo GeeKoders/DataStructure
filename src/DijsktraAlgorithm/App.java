25f6afede8bb54dfe5958fd27d046a3ce26ceff1
GeeKoders <iloveitone@gmail.com>
2020-05-28 22:16:18 +0800

    20200528 add DijsktraAlgorithm

--- src/DijsktraAlgorithm/App.java
+++ src/DijsktraAlgorithm/App.java
@@ -3,8 +3,20 @@ package DijsktraAlgorithm;
 public class App {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Vertex vertex0 = new Vertex("A") ;
+		Vertex vertex1 = new Vertex("B") ;
+		Vertex vertex2 = new Vertex("C") ;
+		
+		vertex0.addNeigbour(new Edge(1, vertex0, vertex1));
+		vertex0.addNeigbour(new Edge(3, vertex0, vertex2));
+		vertex1.addNeigbour(new Edge(1, vertex1, vertex2));
+		
+		
+		DijsktraAlgorithm algorithm = new DijsktraAlgorithm() ;
+		algorithm.computePaths(vertex0);
+		
+		System.out.println(algorithm.getShortestPathTo(vertex2));
 	}
 
 }
