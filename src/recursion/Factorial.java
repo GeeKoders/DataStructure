3d7155bafbbeb7195c187191da7bdf515a0b6611
GeeKoders <iloveitone@gmail.com>
2020-09-08 20:45:40 +0800

    20200908 add Factorial accumulator

--- src/recursion/Factorial.java
+++ src/recursion/Factorial.java
@@ -5,7 +5,9 @@ public class Factorial {
 	public static void main(String[] args) {
 
 		Factorial factorial = new Factorial() ;
-		System.out.println(factorial.factorial(4)) ;
+//		System.out.println(factorial.factorial(4)) ;
+		
+		System.out.println(factorial.calculateFactorial(4));
 		
 	}
 	
@@ -15,6 +17,19 @@ public class Factorial {
 		
 		return n * factorial(n-1) ;
 		
+	}
+	
+	public int calculateFactorial(int n){
+		return factorial2(1, n) ;
+	}
+
+	private int factorial2(int accumulator, int n) {
+
+		if(n == 1) return accumulator ;
+		
+		return factorial2(accumulator*n, n - 1) ;
+	
+	
 	}
 
 }
