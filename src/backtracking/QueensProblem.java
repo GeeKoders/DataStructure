384d8fc30d23341a45ade55f37070f441884cd32
GeeKoders <iloveitone@gmail.com>
2020-09-10 22:43:28 +0800

    20200910 add QueensProblem

---
+++ src/backtracking/QueensProblem.java
@@ -0,0 +1,100 @@
+package backtracking;
+
+public class QueensProblem {
+
+	public static void main(String[] args) {
+		QueensProblem problem = new QueensProblem(100) ;
+		problem.solve();
+		
+	}
+	
+	private int [][] chessTable ;
+	private int numOfQueens ;
+	
+	
+	public QueensProblem(int numOfQueens) {
+		this.chessTable = new int[numOfQueens][numOfQueens] ;
+		this.numOfQueens = numOfQueens ;
+	}
+	
+	public void solve(){
+		
+		if(setQueens(0)){
+			printQueens() ;
+		}else{
+			System.out.println("There is no solutions...");
+		}
+		
+		
+	}
+
+	private boolean setQueens(int colIndex) {
+		
+		if(colIndex == numOfQueens) return true ;
+		
+		for(int rowIndex=0; rowIndex < numOfQueens; rowIndex++){
+			
+			if(isPalceValid(rowIndex, colIndex)){
+				
+				chessTable[rowIndex][colIndex] = 1 ;
+				if(setQueens(colIndex + 1)){
+					return true ;
+				}
+				
+				//backtracking 
+				
+				chessTable[rowIndex][colIndex] = 0 ;
+			}
+		}
+		
+		return false;
+	}
+
+	private boolean isPalceValid(int rowIndex, int colIndex) {
+
+		//horizontal check
+		for(int i=0 ; i<colIndex; i++){
+			if(chessTable[rowIndex][i] == 1){
+				return false ;
+			}
+		}
+		
+		//diagonal check
+		for(int i=rowIndex, j=colIndex; i>=0 && j>=0; i--, j--){
+			if(chessTable[i][j] == 1){
+				return false ;
+			}
+		}
+		
+		for(int i=rowIndex, j=colIndex; i<chessTable.length && j>=0; i++, j--){
+			if(chessTable[i][j] == 1){
+				return false ;
+			}
+		}
+		
+		
+		return true;
+	}
+
+	private void printQueens() {
+		
+		for(int i=0; i<chessTable.length; i++){
+			for(int j=0; j<chessTable.length; j++){
+				if(chessTable[i][j] == 1){
+					System.out.print(" * ");
+				}else{
+					System.out.print(" - ");
+				}
+			}
+			
+			System.out.println();
+		}
+		
+		System.out.println();
+		
+	}
+	
+	
+	
+
+}
