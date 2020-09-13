07e7214ee9b245eeec8decaae37e283bf997afae
GeeKoders <iloveitone@gmail.com>
2020-09-13 14:40:03 +0800

    20200913 add Coin change dp

--- src/DynamicProgramming/CoinChange.java
+++ src/DynamicProgramming/CoinChange.java
@@ -8,7 +8,8 @@ public class CoinChange {
 		int M = 4 ;
 		CoinChange change = new CoinChange() ;
 		
-		System.out.println(change.naiveCoinChange(M, v, 0));
+//		System.out.println(change.naiveCoinChange(M, v, 0));
+		change.dynamicProgrammingCoinChange(v, M) ;
 		
 	}
 	
@@ -20,6 +21,35 @@ public class CoinChange {
 		if(v.length == index ) return 0 ;
 		// we take coin + we don't take coin
 		return naiveCoinChange(M - v[index], v, index) + naiveCoinChange(M, v, index + 1) ;
+		
+	}
+	
+	public void dynamicProgrammingCoinChange(int[] v, int M){
+		
+		int [][] dpTable = new int[v.length + 1][M + 1] ;
+		
+		for(int i=0; i<=v.length; i++){
+			dpTable[i][0] = 1 ;
+		}
+		
+		for(int i=1; i<=M; i++){
+			dpTable[0][i] = 0 ;
+		}
+		
+		//O(v*M)
+		for(int i=1; i<=v.length; i++){
+			for(int j=1; j<=M; j++){
+				
+				if(v[i - 1] <= j){
+					dpTable[i][j] = dpTable[i-1][j] + dpTable[i][j-v[i-1]] ;
+				}else{
+					dpTable[i][j] = dpTable[i-1][j] ;
+				}
+			}
+		}
+		
+		
+		System.out.println("Solution: " + dpTable[v.length][M]);
 		
 	}
 
