b2081974a9dd90652cd607ec435e489d1295fd35
GeeKoders <iloveitone@gmail.com>
2020-09-17 21:14:04 +0800

    20200917 add Bin packing problem finish

--- src/BinPackingProblem/App.java
+++ src/BinPackingProblem/App.java
@@ -1,7 +1,18 @@
 package BinPackingProblem;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class App {
+
 	public static void main(String[] args) {
+		
+		List<Integer> items = Arrays.asList(5,3,5);
+		int binCapacity = 10;
+		
+		FirstFitDecreasingAlgorithm algorithm = new FirstFitDecreasingAlgorithm(items, binCapacity);
+		algorithm.solveBinPackingProblem();
+		algorithm.showResults();
 
 	}
 }
