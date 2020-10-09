64baae545a3c17bc50a2c3a7af0461c42dad236b
GeeKoders <iloveitone@gmail.com>
2020-10-09 11:21:08 +0800

    20201009 add AvlTree height and balance

--- src/Practice/Tree/AVL/AvlTree.java
+++ src/Practice/Tree/AVL/AvlTree.java
@@ -1,15 +1,70 @@
 package Practice.Tree.AVL;
 
-public class AvlTree<T> implements Tree<T> {
+public class AvlTree<T extends Comparable<T>> implements Tree<T> {
 
+	private Node<T> root ;
+	
 	@Override
 	public void insert(T data) {
+		
+		if(root == null){
+			root = new Node<T>(data) ;
+		}else{
+			insert(root, data) ;
+		}
+		
+	}
+	
+	public void insert(Node<T> curr, T data){
+		
+		if(data.compareTo(curr.getData()) < 0){
+			
+			if(curr.getLeftChild() == null){
+				Node<T> newNode = new Node<T>(data) ;
+				curr.setLeftChild(newNode);
+			}else{
+				insert(curr.getLeftChild(), data) ; 
+			}
+			
+		}else{
+			
+			if(curr.getRightChild() == null){
+				Node<T> newNode = new Node<T>(data) ;
+				curr.setRightChild(newNode);
+			}else{
+				insert(curr.getRightChild(), data) ;
+			}
+		}
 		
 	}
 
 	@Override
 	public void traverse() {
+		if(root == null) return ;
+		inOrderTraversal(root) ;
+	}
+	
+	public void inOrderTraversal(Node<T> root){
 		
+		if(root.getLeftChild()!=null){
+			inOrderTraversal(root.getLeftChild()) ;
+		}
+		
+		System.out.println(root);
+		
+		if(root.getRightChild()!=null){
+			inOrderTraversal(root.getRightChild()) ;
+		}
+	}
+	
+	public int height(Node<T> curr){
+		if(curr == null) return -1 ;
+		return curr.getHeight() ;
+	}
+	
+	public int getBalance(Node<T> curr){
+		if(curr == null) return 0 ;
+		return height(curr.getLeftChild()) - height(curr.getRightChild()) ;
 	}
 
 	@Override
