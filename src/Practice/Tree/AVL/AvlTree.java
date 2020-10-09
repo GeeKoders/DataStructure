4445e64350d333851f43d7d9a140e7ee74dcf894
GeeKoders <iloveitone@gmail.com>
2020-10-09 10:04:00 +0800

    20201009 add AVL tree interface

---
+++ src/Practice/Tree/AVL/AvlTree.java
@@ -0,0 +1,20 @@
+package Practice.Tree.AVL;
+
+public class AvlTree<T> implements Tree<T> {
+
+	@Override
+	public void insert(T data) {
+		
+	}
+
+	@Override
+	public void traverse() {
+		
+	}
+
+	@Override
+	public void delete(T data) {
+		
+	}
+
+}
---
+++ src/Practice/Tree/AVL/Tree.java
@@ -0,0 +1,11 @@
+package Practice.Tree.AVL;
+
+public interface Tree<T> {
+	
+	public void insert(T data) ;
+	
+	public void traverse() ;
+	
+	public void delete(T data) ;
+	
+}
