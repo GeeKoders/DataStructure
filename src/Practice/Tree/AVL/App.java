19f82fd241554c71ffe967e57424380b17a9c273
GeeKoders <iloveitone@gmail.com>
2020-10-10 16:56:31 +0800

    20201010 add avl deletion test

--- src/Practice/Tree/AVL/App.java
+++ src/Practice/Tree/AVL/App.java
@@ -6,14 +6,24 @@ public class App {
 
 		Tree<Integer> tree = new AvlTree<>() ;
 		
+//		tree.insert(10);
+//		tree.insert(20);
+//		tree.insert(30);
+//		tree.insert(40);
+//		tree.insert(50);
+//		tree.insert(60);
+//		
+//		tree.traverse();
+		
 		tree.insert(10);
-		tree.insert(20);
-		tree.insert(30);
-		tree.insert(40);
-		tree.insert(50);
-		tree.insert(60);
+		tree.insert(15);
+		tree.insert(5);
+		tree.insert(6);
+		
+		tree.delete(15) ;
 		
 		tree.traverse();
+		
 		
 	}
 
