375e68058aee02163054486cd82c5107092bb3a0
GeeKoders <iloveitone@gmail.com>
2020-10-11 14:16:56 +0800

    20201011 add reb black tree fixViolations

--- src/Practice/Tree/RedBlackTree/RedBlackTree.java
+++ src/Practice/Tree/RedBlackTree/RedBlackTree.java
@@ -1,5 +1,7 @@
 package Practice.Tree.RedBlackTree;
 
+import RedBlackTree.NodeColor;
+
 public class RedBlackTree implements Tree {
 
 	private Node root ;
@@ -39,6 +41,83 @@ public class RedBlackTree implements Tree {
 			root = insertIntoTree(root, data) ; 
 		}
 		
+		fixVioloations(root) ;
+		
+	}
+
+	private void fixVioloations(Node curr) {
+
+		Node parentNode = null ;
+		Node grandParentNode = null ;
+		
+		while( curr != root && curr.getColor() != NodeColor.BLACK && curr.getParent().getColor() == NodeColor.RED ) {
+			
+			parentNode = curr.getParent() ;
+			grandParentNode = curr.getParent().getParent() ;
+			
+			if(parentNode == grandParentNode.getLeftChild()){
+				
+				Node uncle = grandParentNode.getRightChild() ;
+				//case I && case VI
+				if(uncle != null && uncle.getColor() == NodeColor.RED){
+					grandParentNode.setColor(NodeColor.RED);
+					parentNode.setColor(NodeColor.BLACK);
+					uncle.setColor(NodeColor.BLACK);
+					curr = grandParentNode ;
+				}else{
+					
+					if(curr == parentNode.getRightChild()){
+						leftRotate(parentNode) ;
+						//update the reference
+						curr = parentNode ;
+						parentNode = curr.getParent() ;
+					}
+					//case III
+					rightRotate(grandParentNode) ;
+					NodeColor tempColor = parentNode.getColor() ;
+					parentNode.setColor(grandParentNode.getColor());
+					grandParentNode.setColor(tempColor);
+					curr = parentNode ;
+					
+					
+					
+				}
+				
+				
+			}else{
+				
+				Node uncle = grandParentNode.getLeftChild() ;
+				
+				if(uncle != null && uncle.getColor() == NodeColor.RED){
+					grandParentNode.setColor(NodeColor.RED);
+					parentNode.setColor(NodeColor.BLACK);
+					uncle.setColor(NodeColor.BLACK);
+					curr = grandParentNode ;
+				}else{
+					//case II
+					if(curr == parentNode.getLeftChild()){
+						rightRotate(parentNode) ;
+						System.out.println("Recoroling "+parentNode+" + "+grandParentNode);
+						//update the reference
+						curr = parentNode ;
+						parentNode = curr.getParent() ;
+					}
+					
+					leftRotate(grandParentNode) ;
+					System.out.println("Recoroling "+parentNode+" + "+grandParentNode);
+					NodeColor tempColor = parentNode.getColor() ;
+					parentNode.setColor(grandParentNode.getColor());
+					grandParentNode.setColor(tempColor);
+					curr = parentNode ;
+				}
+			}
+			
+			if(root.getColor() == NodeColor.RED){
+				System.out.println("Recoring the root to the black...");
+				root.setColor(NodeColor.BLACK) ;
+			}
+			
+		}
 	}
 
 	private Node insertIntoTree(Node curr, int data) {
