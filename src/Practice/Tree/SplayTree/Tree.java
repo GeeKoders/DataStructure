18b545486a8c4117804129fa00dc542642573152
GeeKoders <iloveitone@gmail.com>
2020-10-17 09:33:48 +0800

    20201017 add splay tree tree interface

--- src/Practice/Tree/SplayTree/Tree.java
+++ src/Practice/Tree/SplayTree/Tree.java
@@ -1,5 +1,15 @@
 package Practice.Tree.SplayTree;
 
-public class Tree {
+public interface Tree <T extends Comparable<T>> {
 
+	public void insert(T data) ;
+	
+	public Node<T> find(T data) ;
+	
+	public T getMin() ;
+	
+	public T getMax() ;
+	
+	public void inOrderTraversal() ;
+	
 }
