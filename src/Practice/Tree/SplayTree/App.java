a5e7d42b298309c627e1bc9bbcc607f3303c6c47
GeeKoders <iloveitone@gmail.com>
2020-10-17 09:31:13 +0800

    20201017 add splay tree node

---
+++ src/Practice/Tree/SplayTree/App.java
@@ -0,0 +1,10 @@
+package Practice.Tree.SplayTree;
+
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/Practice/Tree/SplayTree/Tree.java
@@ -0,0 +1,5 @@
+package Practice.Tree.SplayTree;
+
+public class Tree {
+
+}
