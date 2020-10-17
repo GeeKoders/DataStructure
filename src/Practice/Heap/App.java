8896f5d0e6b6f6cab15ffea8d640de165788ba7d
GeeKoders <iloveitone@gmail.com>
2020-10-17 14:53:27 +0800

    20201017 add heap test

--- src/Practice/Heap/App.java
+++ src/Practice/Heap/App.java
@@ -3,9 +3,29 @@ package Practice.Heap;
 public class App {
 
 	public static void main(String[] args) {
-		
-		Heap heap = new Heap() ;
-		
+
+		Heap heap = new Heap();
+
+		heap.insert(10);
+		heap.insert(8);
+		heap.insert(12);
+		heap.insert(20);
+		heap.insert(-2);
+		heap.insert(0);
+		heap.insert(1);
+		heap.insert(321);
+
+		System.out.println(heap.poll());
+		System.out.println(heap.poll());
+		System.out.println(heap.poll());
+		System.out.println(heap.poll());
+		System.out.println(heap.poll());
+		System.out.println(heap.poll());
+		System.out.println(heap.poll());
+		System.out.println(heap.poll());
+
+		heap.heapSort();
+
 	}
 
 }
