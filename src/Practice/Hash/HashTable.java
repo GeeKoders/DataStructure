202a29ae29eaac35a98a4effbc6a0c4c23684aeb
GeeKoders <iloveitone@gmail.com>
2020-10-21 21:49:32 +0800

    20201021 add HashTable get and put method

--- src/Practice/Hash/HashTable.java
+++ src/Practice/Hash/HashTable.java
@@ -10,10 +10,49 @@ public class HashTable {
 	}
 	
 	public int get(int key){
+		int index = hash(key) ;
+		
+		if(hashTable[index] == null ){
+			return -1 ;
+		}else{
+			HashItem hashItem = hashTable[index] ;
+			
+			while(hashItem!=null && hashItem.getKey()!=key){
+				hashItem = hashItem.getNextHashItem() ;
+			}
+			
+			if(hashItem == null){
+				return -1 ;
+			}else{
+				return hashItem.getValue() ;
+			}
+		}
+		
+		
 		
 	}
 	
 	public void put(int key, int value){
+		
+		int index = hash(key) ;
+		
+		if(hashTable[index] == null){
+			System.out.println("No collision simple insertion...");
+			hashTable[index] = new HashItem(key, value) ;
+		}else{
+			
+			System.out.println("Collision with key " + key);
+			HashItem hashItem = hashTable[index] ;
+			
+			while(hashItem.getNextHashItem()!=null){
+				hashItem = hashItem.getNextHashItem() ;
+				System.out.println("Consider the next item in linked list " + hashItem.getValue());
+			}
+			
+			System.out.println("Finally we have found the place to insert...");
+			hashItem.setNextHashItem(new HashItem(key, value));
+		}
+		
 		
 	}
 	
