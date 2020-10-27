4d71d304fe964df5b61c733f690f492e9cf4fca3
GeeKoders <iloveitone@gmail.com>
2020-10-27 21:52:47 +0800

    20201027 add hashTable generic basic

---
+++ src/Practice/Hash/Generic/Constants.java
@@ -0,0 +1,7 @@
+package Practice.Hash.Generic;
+
+public class Constants {
+
+	
+	public static final int TABLE_SIZE = 10 ;
+}
---
+++ src/Practice/Hash/Generic/HashTable.java
@@ -0,0 +1,38 @@
+package Practice.Hash.Generic;
+
+public class HashTable<Key, Value> {
+	
+	private Key[] keys ;
+	private Value[] values ;
+	private int numOfItems ;
+	private int capacity ;
+	
+	public HashTable() {
+		this.keys = (Key[])new Object[Constants.TABLE_SIZE] ;
+		this.values = (Value[])new Object[Constants.TABLE_SIZE] ;
+		this.capacity = Constants.TABLE_SIZE ;
+		this.numOfItems = 0 ;
+	}
+	
+	public HashTable(int newCapacity){
+		this.keys = (Key[])new Object[newCapacity] ;
+		this.values = (Value[])new Object[newCapacity] ;
+		this.capacity = newCapacity ;
+		this.numOfItems = 0 ;
+	}
+	
+	public int size(){
+		return this.numOfItems ;
+	}
+	
+	public boolean isEmpty(){
+		return this.numOfItems == 0 ;
+	}
+	
+	
+		
+	
+	
+	
+
+}
