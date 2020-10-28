0c79c02fe07ece1fe09455d6ac6f855a175303d3
GeeKoders <iloveitone@gmail.com>
2020-10-29 07:30:45 +0800

    20201029 add Trie basic structure

---
+++ src/Practice/Tries/Constant.java
@@ -0,0 +1,7 @@
+package Practice.Tries;
+
+public class Constant {
+
+	public static final int ALPHABET_SIZE = 26 ;
+	
+}
---
+++ src/Practice/Tries/Trie.java
@@ -0,0 +1,19 @@
+package Practice.Tries;
+
+public class Trie {
+
+	private Node node ;
+	
+	public Trie() {
+		this.node = new Node("") ;
+	}
+	
+	public void insert(String word){
+		
+	}
+	
+	public boolean search(String word){
+		
+	}
+	
+}
