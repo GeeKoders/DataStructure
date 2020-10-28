d0985eb6b03106f3a6523449ddcd6b90c547448f
GeeKoders <iloveitone@gmail.com>
2020-10-28 08:23:10 +0800

    20201028 add twosum hashtable solution

--- src/Practice/Hash/interview/App.java
+++ src/Practice/Hash/interview/App.java
@@ -1,5 +1,7 @@
 package Practice.Hash.interview;
 
+import twosum.DynamicProgrammingTwoSum;
+
 public class App {
 
 	public static void main(String[] args) {
@@ -7,8 +9,11 @@ public class App {
 		int []nums = {3, 5, 2, -4, 8, 11} ;
 		int S = 7 ;
 		
-		NativeSolution nativeSolution = new NativeSolution(nums, S) ;
-		nativeSolution.solve() ;
+//		NativeSolution nativeSolution = new NativeSolution(nums, S) ;
+//		nativeSolution.solve() ;
+		
+		DynamicProgrammingTwoSum dynamicProgrammingTwoSum = new DynamicProgrammingTwoSum(nums, S) ;
+		dynamicProgrammingTwoSum.solve();
 	}
 
 }
--- src/Practice/Hash/interview/DynamicProgrammingTwoSum.java
+++ src/Practice/Hash/interview/DynamicProgrammingTwoSum.java
@@ -1,5 +1,38 @@
 package Practice.Hash.interview;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class DynamicProgrammingTwoSum {
 
+	
+	private int[] nums ;
+	
+	private int S; 
+	
+	private Map<Integer, Integer> hashTable ;
+	
+	public DynamicProgrammingTwoSum(int[] nums, int S) {
+		this.nums = nums ;
+		this.S = S ;
+		this.hashTable = new HashMap<>() ;
+	}
+	
+	public void solve(){
+		
+		for(int i=0; i<nums.length; i++){
+			
+			int reminder = S - nums[i] ;
+			
+			if(hashTable.containsValue(reminder)){
+				System.out.println("Solution: " + nums[i] + "+" + reminder + "=" + S);
+			}
+			
+			hashTable.put(i, nums[i]) ;
+			
+			
+		}
+		
+	}
+	
 }
