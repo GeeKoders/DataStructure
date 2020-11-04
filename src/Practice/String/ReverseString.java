9f687db465584a26b97ce80fcb424862378e66bb
GeeKoders <iloveitone@gmail.com>
2020-11-04 21:14:03 +0800

    20201104 add reverse String solution2

--- src/Practice/String/ReverseString.java
+++ src/Practice/String/ReverseString.java
@@ -5,7 +5,9 @@ public class ReverseString {
 	public static void main(String[] args) {
 
 		ReverseString reverseString = new ReverseString() ;
-		System.out.println(reverseString.solution1("school"));
+//		System.out.println(reverseString.solution1("school"));
+		
+		System.out.println(reverseString.solution2("school"));
 	}
 	//O(N)
 	public String solution1(String text){
@@ -21,6 +23,31 @@ public class ReverseString {
 		
 		return result.toString() ;
 		
+	}
+	
+	public String solution2(String text){
+		
+		char[] textChar= text.toCharArray() ;
+		int left = 0 ;
+		int right = textChar.length - 1 ;
+		while(left<=right){
+			
+			swap(left, right, textChar) ;
+			left++ ;
+			right-- ;
+		}
+		StringBuilder sb = new StringBuilder() ;
+		for(int i=0; i<textChar.length; i++){
+			sb.append(textChar[i]) ;
+		}
+		
+		return sb.toString() ;
+		
+	}
+	private void swap(int left, int right, char[] textChar) {
+		char tmp = textChar[left] ;
+		textChar[left] = textChar[right] ;
+		textChar[right] = tmp ;
 	}
 
 }
