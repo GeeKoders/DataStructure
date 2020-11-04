731075738ff535c201c874f8f567aa218a82ef7c
GeeKoders <iloveitone@gmail.com>
2020-11-04 21:28:19 +0800

    20201104 add StringOperations getPrefixes

---
+++ src/Practice/String/StringOperations.java
@@ -0,0 +1,31 @@
+package Practice.String;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StringOperations {
+	
+	public static void main(String[] args) {
+		
+		StringOperations so = new StringOperations() ;
+		
+		List<String> result = so.getPrefixes("house") ;
+		
+		for(String item: result){
+			System.out.println(item);
+		}
+		
+	}
+	
+	public List<String> getPrefixes(String text){
+		
+		List<String> list = new ArrayList<>() ;
+		
+		for(int index=0;index<text.length();index++){
+			list.add(text.substring(0, index+1)) ;
+		}
+		return list ;
+		
+	}
+	
+}
