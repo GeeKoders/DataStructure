566bdca4d6d0bdfc177c54738981c0d98c3d8349
GeeKoders <iloveitone@gmail.com>
2020-11-05 22:51:24 +0800

    20201105 add Bogo sort

---
+++ src/Practice/Sorting/BogoSort/App.java
@@ -0,0 +1,54 @@
+package Practice.Sorting.BogoSort;
+
+public class App {
+
+	public static void main(String[] args) {
+
+		App app = new App() ;
+		
+		int[] nums= {12, -4, 56, 0, 3, 8, 2, 1} ;
+		int counter = 0 ;
+		
+		while(!app.isSorted(nums)){
+			counter++ ;
+			app.shuffle(nums) ;
+			
+		}
+		
+		System.out.println("Steps required to sort array: " + counter);
+		app.showArray(nums);
+	}
+	
+	public void shuffle(int[]nums){
+		
+		for(int i=0; i<nums.length; i++){
+			int j = (int)(Math.random() * i) ;
+			swap(nums, i, j) ;
+		}
+	}
+	
+	public boolean isSorted(int[] nums){
+		
+		for(int i=0; i<nums.length - 1; i++){
+			
+			if(nums[i] > nums[i+1]){
+				return false ;
+			}
+		}
+		
+		return true ;
+	}
+	
+	public void swap(int[]nums, int i, int j){
+		int temp = nums[i] ;
+		nums[i] = nums[j] ;
+		nums[j] = temp ;
+	}
+	
+	public void showArray(int[] nums){
+		
+		for(int i=0; i<nums.length; i++){
+			System.out.print(nums[i] + " ");
+		}
+	}
+}
