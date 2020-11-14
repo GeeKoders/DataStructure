49b7ec701b2be389f32eaf90adfe84e3d6a5b1a4
GeeKoders <iloveitone@gmail.com>
2020-11-14 16:33:18 +0800

    20201114 add RLE decode

--- src/Practice/DataCompression/RunLengthEncoding/RunLengthEncoding.java
+++ src/Practice/DataCompression/RunLengthEncoding/RunLengthEncoding.java
@@ -1,12 +1,15 @@
 package Practice.DataCompression.RunLengthEncoding;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class RunLengthEncoding {
 
 	public static void main(String[] args) {
 
 		RunLengthEncoding rle = new RunLengthEncoding() ;
-		System.out.println(rle.encode("AAABB"));
-		
+//		System.out.println(rle.encode("AAABB"));
+		System.out.println(rle.decode("4A3B2C"));
 		
 	}
 	
@@ -30,6 +33,29 @@ public class RunLengthEncoding {
 		}
 		
 		return sb.toString() ;
+	}
+	
+	public static String decode(String compressText) {
+		// non thread-safe
+		StringBuilder stringBuilder = new StringBuilder();
+
+		Pattern pattern = Pattern.compile("[0-9]+|[a-zA-z]");
+		Matcher matcher = pattern.matcher(compressText);
+
+		while (matcher.find()) {
+
+			int runLength = Integer.parseInt(matcher.group());
+			System.out.println("group:" + matcher.group());
+			matcher.find();
+
+			while (runLength-- != 0) {
+				stringBuilder.append(matcher.group()); // 4A -> AAAA
+			}
+
+		}
+
+		return stringBuilder.toString();
+
 	}
 
 }
