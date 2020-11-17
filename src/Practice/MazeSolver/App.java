a98dc91267a95d59c281c826c3714d03db6fbf23
GeeKoders <iloveitone@gmail.com>
2020-11-17 21:02:42 +0800

    20201117 fix MazeSolver

--- src/Practice/MazeSolver/App.java
+++ src/Practice/MazeSolver/App.java
@@ -4,7 +4,7 @@ public class App {
 
 	public static void main(String[] args) {
 
-		FileReader fileReader = new FileReader("D:\\workspace\\DataStructure\\src\\MazeSolver\\map.txt", 5, 5) ;
+		FileReader fileReader = new FileReader("D:\\workspace\\DataStructure\\src\\Practice\\MazeSolver\\map.txt", 5, 5) ;
 		fileReader.parseFile(); 
 		MazeSolver mazeSolver = new MazeSolver(fileReader.getMpa(), fileReader.getStartPositionCol(), fileReader.getStartPositionRow()) ;
 		mazeSolver.findWayOut();
