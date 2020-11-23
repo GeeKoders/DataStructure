983c8422466b03633199f7688a6498b978ff6332
GeeKoders <iloveitone@gmail.com>
2020-11-24 07:33:16 +0800

    20201124 add PrimAlgorithm app test

--- src/Practice/Graph/PrimAlgorithm/App.java
+++ src/Practice/Graph/PrimAlgorithm/App.java
@@ -1,10 +1,34 @@
 package Practice.Graph.PrimAlgorithm;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class App {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		List<Vertex> vertexList = new ArrayList<>() ;
+		
+		Vertex vertex0 = new Vertex("A") ;
+		Vertex vertex1 = new Vertex("B") ;
+		Vertex vertex2 = new Vertex("C") ;
+		
+		vertexList.add(vertex0) ;
+		vertexList.add(vertex1) ;
+		vertexList.add(vertex2) ;
+		
+		vertex0.addEdge(new Edge(vertex0, vertex1, 1));	
+		vertex0.addEdge(new Edge(vertex0, vertex2, 1));	
+		vertex1.addEdge(new Edge(vertex1, vertex2, 10));
+		
+		vertex1.addEdge(new Edge(vertex1, vertex0, 1));	
+		vertex2.addEdge(new Edge(vertex2, vertex0, 1));	
+		vertex2.addEdge(new Edge(vertex2, vertex1, 10));
+		
+		Algorithm algorithm = new Algorithm(vertexList) ;
+		
+		algorithm.primsAlgorithm(vertex2);
+		algorithm.showMST();
 	}
 
 }
