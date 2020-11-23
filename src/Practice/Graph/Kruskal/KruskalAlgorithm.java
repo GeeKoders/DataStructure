84753f91e7a4b1dbeaa19daedfb90aa869410b80
GeeKoders <iloveitone@gmail.com>
2020-11-23 21:20:00 +0800

    20201123 add KruskalAlgorithm spanning trees

--- src/Practice/Graph/Kruskal/KruskalAlgorithm.java
+++ src/Practice/Graph/Kruskal/KruskalAlgorithm.java
@@ -1,5 +1,34 @@
 package Practice.Graph.Kruskal;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class KruskalAlgorithm {
 
+	public void spanningTree(List<Vertex> vertexList, List<Edge> edgeList){
+		
+		DisjoinSet disjoinSet = new DisjoinSet(vertexList) ;
+		
+		List<Edge> mst = new ArrayList<Edge>() ;
+		
+		Collections.sort(edgeList) ;
+		
+		for(Edge edge: edgeList){
+			Vertex u = edge.getStartVertex() ;
+			Vertex v = edge.getTargetVertex() ;
+			
+			if(disjoinSet.find(u.getNode())!= disjoinSet.find(v.getNode())){
+				mst.add(edge) ;
+				disjoinSet.union(u.getNode(), v.getNode()) ;
+			}
+			
+		}
+		
+		for(Edge edge: msy){
+			System.out.println(edge.getStartVertex() + " " + edge.getTargetVertex() + " -- ");
+		}
+		
+	}
+	
 }
