db412b8df02832646770e79a24f00927312fe53c
GeeKoders <iloveitone@gmail.com>
2020-11-29 13:37:20 +0800

    20201129 add FlowNetwork

--- src/Practice/Graph/MaxFlow/FlowNetwork.java
+++ src/Practice/Graph/MaxFlow/FlowNetwork.java
@@ -1,5 +1,43 @@
 package Practice.Graph.MaxFlow;
 
-public class FlowNetwork {
+import java.util.ArrayList;
+import java.util.List;
 
+public class FlowNetwork {
+	
+	private int numOfVertices ;
+	private int numOfEdges ;
+	private List<List<Edge>> adjacenciesList ;
+
+	public FlowNetwork(int numOfVertices) {
+		this.numOfVertices = numOfVertices ;
+		this.numOfEdges = 0 ;
+		this.adjacenciesList = new ArrayList<>() ;
+		// avoid to null pointer exception
+		for(int i=0; i<numOfVertices; i++){
+			List<Edge> edgeList = new ArrayList<>() ;
+			this.adjacenciesList.add(edgeList) ;
+		}
+		
+	}
+	
+	public int getNumOfVertices(){
+		return this.numOfVertices ;
+	}
+	
+	public int getNumOfEdges(){
+		return this.numOfEdges ;
+	}
+	
+	public void addEdge(Edge e){
+		Vertex v = e.getFromVertex() ;
+		Vertex w = e.getTargetVertex() ;
+		adjacenciesList.get(v.getId()).add(e) ;
+		adjacenciesList.get(w.getId()).add(e) ;
+		numOfEdges ++ ;
+	}
+	
+	public List<Edge> getAdjacenciesList(Vertex v){
+		return adjacenciesList.get(v.getId()) ;
+	}
 }
