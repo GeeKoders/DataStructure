f806a8f49f985989379bd3a1d6fa8c561839aaa1
GeeKoders <iloveitone@gmail.com>
2020-11-30 22:27:23 +0800

    20201130 add SimulatedAnnealing

--- src/Practice/Graph/SimulatedAnnealing/SimulatedAnnealing.java
+++ src/Practice/Graph/SimulatedAnnealing/SimulatedAnnealing.java
@@ -2,4 +2,61 @@ package Practice.Graph.SimulatedAnnealing;
 
 public class SimulatedAnnealing {
 
+	private SingleTour best ;
+	
+	public SimulatedAnnealing(){
+		
+	}
+	
+	public void simulation(){
+		
+		double temperature = 10000 ;
+		double coolingRate = 0.003 ;
+		
+		SingleTour currentSolution = new SingleTour() ;
+		currentSolution.generateIndividual();
+		
+		System.out.println("Initial solution distance: " + currentSolution.getDistance());
+		
+		best = new SingleTour(currentSolution.getTour()) ;
+		
+		while(temperature > 1){
+			
+			SingleTour newSolution = new SingleTour(currentSolution.getTour()) ;
+			
+			int randGeneratedIndex1 = (int)(newSolution.getTourSize() * Math.random()) ;
+			City city1 = newSolution.getCity(randGeneratedIndex1) ;
+			
+			int randGeneratedIndex2 = (int)(newSolution.getTourSize() * Math.random()) ;
+			City city2 = newSolution.getCity(randGeneratedIndex2) ;
+			
+			newSolution.setCity(randGeneratedIndex2, city1) ;
+			newSolution.setCity(randGeneratedIndex1, city2) ;
+			
+			double currentEnergy = currentSolution.getDistance() ;
+			double neighbourEnergy = newSolution.getDistance() ;
+			
+			if(acceptanceProbability(currentEnergy, neighbourEnergy, temperature) > Math.random()){
+				currentSolution = new SingleTour(currentSolution.getTour()) ;
+			}
+			
+			if(currentSolution.getDistance() < best.getDistance()){
+				best = new SingleTour(currentSolution.getTour()) ;
+			}
+			
+			temperature *= 1 - coolingRate ;
+		}
+		
+	}
+
+	private double acceptanceProbability(double currentEnergy,
+			double neighbourEnergy, double temperature) {
+		
+		if(neighbourEnergy < currentEnergy){
+			return 1 ;
+		}
+		
+		return Math.exp( (currentEnergy - neighbourEnergy)/temperature);
+	}
+	
 }
--- src/Practice/Graph/SimulatedAnnealing/SingleTour.java
+++ src/Practice/Graph/SimulatedAnnealing/SingleTour.java
@@ -73,7 +73,7 @@ public class SingleTour {
 		
 	}
 
-	private void setCity(int cityIndex, City city) {
+	public void setCity(int cityIndex, City city) {
 		this.tour.set(cityIndex, city) ;
 		this.distance = 0 ;
 	}
