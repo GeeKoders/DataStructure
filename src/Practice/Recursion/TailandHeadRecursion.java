cd020b76622a0bb85dcf463117af378216fb7636
GeeKoders <iloveitone@gmail.com>
2020-12-01 21:32:03 +0800

    20201201 add TailandHeadRecursion

---
+++ src/Practice/Recursion/TailandHeadRecursion.java
@@ -0,0 +1,33 @@
+package Practice.Recursion;
+
+public class TailandHeadRecursion {
+
+	public static void main(String[] args) {
+
+		TailandHeadRecursion tailandHeadRecursion = new TailandHeadRecursion() ;
+		
+//		tailandHeadRecursion.tail(5);
+		tailandHeadRecursion.head(5);
+	}
+	//descending order
+	public void tail(int N){
+		
+		if(N == 0) return ;
+		
+		System.out.println(N);
+		
+		tail(N-1) ;
+		
+	}
+	//ascending order
+	public void head(int N){
+		
+		if(N == 0) return ;
+		
+		head(N - 1) ;
+		
+		System.out.println(N);
+	}
+	
+	
+}
