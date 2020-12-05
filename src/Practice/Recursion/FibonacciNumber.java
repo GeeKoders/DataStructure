edfb3f9dd4047865c5e634febcd23ddb9a99f934
GeeKoders <iloveitone@gmail.com>
2020-12-05 22:04:42 +0800

    20201205 add FibonacciNumber solve2

--- src/Practice/Recursion/FibonacciNumber.java
+++ src/Practice/Recursion/FibonacciNumber.java
@@ -5,7 +5,8 @@ public class FibonacciNumber {
 	public static void main(String[] args) {
 		
 		FibonacciNumber fibonacciNumber = new FibonacciNumber() ;
-		System.out.println(fibonacciNumber.solve1(6)); 
+		System.out.println(fibonacciNumber.solve1(7)); 
+		System.out.println(fibonacciNumber.solve2(7)); 
 		
 	}
 	
@@ -14,6 +15,16 @@ public class FibonacciNumber {
 		if(n <= 1) return n ;
 		
 		return solve1(n-2) + solve1(n-1) ;
+		
+	}
+	
+	public int solve2(int n){
+		
+		if(n <= 1) return n ;
+		
+		int result = solve1(n-2) + solve1(n-1) ;
+		
+		return result ;
 		
 	}
 	
