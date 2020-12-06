245a53ad458966892ceac70eec1f190a6c83d0ac
GeeKoders <iloveitone@gmail.com>
2020-12-06 13:20:34 +0800

    20201206 add BinarySearch recursive

--- src/Practice/Search/BinarySearch.java
+++ src/Practice/Search/BinarySearch.java
@@ -7,8 +7,8 @@ public class BinarySearch {
 		BinarySearch binarySearch = new BinarySearch(); 
 		int []nums = {-5, 1, 3, 5, 10, 100} ;
 		
-		System.out.println(binarySearch.find(nums, 6));
-		
+//		System.out.println(binarySearch.find(nums, 6));
+		System.out.println(binarySearch.find2(nums, 3, 0, nums.length-1));
 	}
 	
 	public int find(int[] nums, int item){
@@ -29,6 +29,30 @@ public class BinarySearch {
 			}
 		}
 		return -1 ;
+	}
+	
+	public int find2(int[] container, int item, int left, int right){
+		
+		//this is the search miss when item is not present in the container
+		if(right < left) return -1 ;
+		
+		int middle_index = (left + right) / 2 ;
+		
+		//we find the item we are looking for
+		if(container[middle_index] == item) return middle_index ;
+		
+		//we have to check the item we are looking for is smaller or
+		//greater than the middle_item
+		if(container[middle_index] < item){
+			//we can discard the left sub-array
+			return find2(container, item, middle_index+1, right) ;
+		}else{
+			//item is smaller than the middle_item
+			//we can discard the right sub-array
+			return find2(container, item, left, middle_index-1) ;
+			
+		}
+		
 	}
 	
 }
