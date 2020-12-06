e8f2713a29f3178827aadd41674d9e1d232796f5
GeeKoders <iloveitone@gmail.com>
2020-12-06 12:54:33 +0800

    20201206 add LinearSearch recursive

--- src/Practice/Search/LinearSearch.java
+++ src/Practice/Search/LinearSearch.java
@@ -6,8 +6,8 @@ public class LinearSearch {
 		
 		LinearSearch linearSearch = new LinearSearch() ;
 		int []nums = {1, 5, -5, 3, 10, 100} ;
-		System.out.println(linearSearch.find(nums, -6));
-		
+//		System.out.println(linearSearch.find(nums, -6));
+		System.out.println(linearSearch.recursive(nums, -6, 0));
 	}
 	
 	
@@ -24,6 +24,17 @@ public class LinearSearch {
 		//this is the search miss when the item is not found
 		return -1 ;
 		
+		
+	}
+	
+	public int recursive(int []container, int item, int index){
+		
+		//search miss
+		if(index >= container.length) return -1 ;
+		
+		if(container[index] == item) return index ;
+		
+		return recursive(container, item, index+1) ;
 		
 	}
 
